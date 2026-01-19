29e256cb83259bf6b2b83a1ce19a5fbcfabefb95
Cyan <124202346+cyan-wolf@users.noreply.github.com>
2026-01-19 19:35:43 -0400

    add token types

---
+++ src/token.zig
@@ -0,0 +1,64 @@
+
+pub const TokenKind = enum {
+    // Parentheses.
+    left_paren,
+    right_paren,
+    left_brace,
+    right_brace,
+
+    // Single character operators.
+    comma,
+    dot,
+    minus,
+    plus,
+    semicolon,
+    slash,
+    star,
+
+    // Equality.
+    bang,
+    bang_equal,
+    equal,
+    double_equal,
+
+    // Comparison operators.
+    greater,
+    greater_equal,
+    less,
+    less_equal,
+
+    // Literals.
+    identifier,
+    string,
+    number,
+
+    // Keywords (prefixed by k_* to avoid conflicts with Zig's keywords)
+    k_and,
+    k_class,
+    k_else,
+    k_false,
+    k_for,
+    k_fun,
+    k_if,
+    k_nil,
+    k_or,
+    k_print,
+    k_return,
+    k_super,
+    k_this,
+    k_true,
+    k_var,
+    k_while,
+
+    // Misc.
+    error_token,
+    eof,
+};
+
+pub const Token = struct {
+    kind: TokenKind,
+    start_idx: usize,
+    len: usize,
+    line: usize,
+};
+
